d7e00dff65f7b6fe41feef9dcca2d256cb1a2c97
santiago <elsantihg@gmail.com>
2024-11-11 18:02:28 -0500

    fix: varibles de desplegar en vercel

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,19 @@ app.use(express.json());
 
 // Configurar CORS
 app.use(cors());
+app.get("/", (req, res) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>Soy un proyecto Back end en vercel</h1>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
+});
 
 
 // Usar el enrutador para las rutas
